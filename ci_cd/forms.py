f54cf9d88df03d16977d605ffbca76475ee4f25a
yazanwa11 <yazanwa@ac.sce.ac.il>
2025-05-21 16:32:18 +0300

    BSPM25T8-33

--- ci_cd/forms.py
+++ ci_cd/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User , Repository
+from .models import User , Repository,Profile,Module,Exercise
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserRegistrationForm(UserCreationForm):
+    is_instructor = forms.BooleanField(required=False, label="Sign up as instructor")
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2", "is_instructor"]
         
 
 class LoginForm(forms.Form):
